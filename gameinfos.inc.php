5d380491bb092aa95c0ce832957fd7635de53f97
Guy Baudouin <guythoun@gmail.com>
2023-08-30 17:03:54 +0200

    change gameinfos numbers

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -85,11 +85,11 @@ $gameinfos = array(
 // Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
 'complexity' => 3,
 
+// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
+'strategy' => 4,
+
 // Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
 'luck' => 2,
-
-// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 3,
 
 // Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
 'diplomacy' => 3,
